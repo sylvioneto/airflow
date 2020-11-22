7e5cb37f725b1e7c8140e0c1749f4a2f977ec4e3
sylvioneto <sylvio.pedroza@gmail.com>
2020-11-22 10:54:08 -0600

    airflow users create

--- airflow-home/startup.bash
+++ airflow-home/startup.bash
@@ -13,7 +13,7 @@ wait_for_it $DB_HOST $DB_PORT
 #!/bin/bash
 if [ $1 = "scheduler" ]; then
     airflow db init
-    airflow create_user --role Admin --username admin --firstname Admin --lastname Default --email admin@myorg.com
+    airflow users create --role Admin --username admin --firstname Admin --lastname Default --email admin@myorg.com
 fi
 
 airflow $1
